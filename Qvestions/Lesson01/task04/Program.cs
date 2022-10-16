0201b9982f74c3bbc372212557c2a14cc201ebbf
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 21:44:14 +0300

    change task04

--- Qvestions/Lesson01/task04/Program.cs
+++ Qvestions/Lesson01/task04/Program.cs
@@ -1,12 +1,11 @@
 ﻿// Условие: Написать программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Введите первое целое число: ");
-int number1 = Convert.ToInt32 (Console.ReadLine());
-Console.Write("Введите второе целое число: ");
-int number2 = Convert.ToInt32 (Console.ReadLine());
-Console.Write("Введите третье целое число: ");
-int number3 = Convert.ToInt32 (Console.ReadLine());
-
+Console.Write("Введите первое  число: ");
+double number1 = Convert.ToDouble (Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToDouble (Console.ReadLine());
+Console.Write("Введите третье  число: ");
+double number3 = Convert.ToDouble (Console.ReadLine());
 
 
 if (number1 > number2 && number3 < number1)
@@ -21,4 +20,3 @@ else if (number3 > number2 && number1 < number3)
 {
    Console.WriteLine($"Число {number3} максимальное");
 }
-else Console.WriteLine("Введены некоректные числа");
