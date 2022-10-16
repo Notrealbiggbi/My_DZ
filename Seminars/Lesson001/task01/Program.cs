0639254f2e96c3cb68a9f6e5762da1ec403b18f5
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 16:36:42 +0300

    добавил решение задачи task01

--- Seminars/Lesson001/task01/Program.cs
+++ Seminars/Lesson001/task01/Program.cs
@@ -6,5 +6,11 @@
 //         -7 -> 49
 
 
-Console.WriteLine("Введите целое число: ");
-int number = Console.ReadLine();
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+// string str = Console.ReadLine();
+// int num = Convert.ToInt32(str);
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
