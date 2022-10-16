e9e65a4dd6430b079e1e27a04c51eb9029a4129f
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 16:52:38 +0300

    написал условие задачи №1

--- Seminars/Lesson001/task02[№1]/Program.cs
+++ Seminars/Lesson001/task02[№1]/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите первое целое число: ");
+﻿//Написать программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Введите первое целое число: ");
 int number1 = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Введите второе целое число: ");
 int number2 = Convert.ToInt32 (Console.ReadLine());
