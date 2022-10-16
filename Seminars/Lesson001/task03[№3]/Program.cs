04588aa8880b37844a1768a425523b5d645b5ab8
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 19:24:32 +0300

    добавил комментарии

--- Seminars/Lesson001/task03[№3]/Program.cs
+++ Seminars/Lesson001/task03[№3]/Program.cs
@@ -8,27 +8,30 @@ if (num == 1)
 {
     Console.WriteLine("Сегодня Понедельник");
 }
-if (num == 2)
+if (num == 2) //else if
 {
     Console.WriteLine("Сегодня Вторник");
 }
-if (num == 3)
+if (num == 3) //else if
+
 {
     Console.WriteLine("Сегодня Среда");
 }
-if (num == 4)
+if (num == 4) //else if
 {
     Console.WriteLine("Сегодня Четверг");
 }
-if (num == 5)
+if (num == 5) //else if
+
 {
     Console.WriteLine("Сегодня Пятница");
 }
-if (num == 6)
+if (num == 6) //else if
+
 {
     Console.WriteLine("Сегодня Суббота");
 }
-if (num == 7)
+if (num == 7) //else if
 {
     Console.WriteLine("Сегодня Воскресенье");
 }
