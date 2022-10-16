5cb8a4d7e3c9bd8abd68b892b8446fde296237bf
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 16:50:48 +0300

    написал решение задачи №1

---
+++ Seminars/Lesson001/task02[№1]/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+int square = number2 * number2;
+
+if (square==number1)
+{
+   Console.WriteLine($"Квадрат числа {number2} равен числу {number1}"); 
+}
+else
+{
+   Console.WriteLine($"Квадрат числа {number2} не равен числу {number1}"); 
+}
