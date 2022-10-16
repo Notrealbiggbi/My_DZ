6786d2e0ec7424b903985771abeab5ff3f81dcf8
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 20:04:22 +0300

    подправил 8 строку

--- Seminars/Lesson001/task04[№5]/Program.cs
+++ Seminars/Lesson001/task04[№5]/Program.cs
@@ -5,7 +5,7 @@
 // 4 -> -4,-3,-2,-1, 0, 1, 2, 3, 4
 // 2 -> -2,-1, 0, 1, 2
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите положительное целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int count = -num;
