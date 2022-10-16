bd24a95dc1385d97205343fa3b22ea1b88c64ac4
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 21:21:25 +0300

    change str 9-14

--- Seminars/Lesson001/task05[№7]/Program.cs
+++ Seminars/Lesson001/task05[№7]/Program.cs
@@ -6,5 +6,9 @@
 Console.WriteLine("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num > 99 && num < 1000)
+{
 int LastDigit = num % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
 Console.WriteLine($"Последней цифрой числа {num} является {LastDigit}");
+}
+else Console.WriteLine("Введено некоректное число");
