16963e02214bc2e3e8c7c2a6a58bcb3d5e9f237b
Vadim <mihailovvadick1996@yandex.ru>
2022-10-16 21:31:53 +0300

    изменил строки 14,16,18,20,22 и добавил 24

--- Qvestions/Lesson01/task04/Program.cs
+++ Qvestions/Lesson01/task04/Program.cs
@@ -7,15 +7,18 @@ int number2 = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Введите третье целое число: ");
 int number3 = Convert.ToInt32 (Console.ReadLine());
 
+
+
 if (number1 > number2 && number3 < number1)
 {
-   Console.WriteLine($"Число {number1} больше чем число {number2} и больше чем число {number3}"); 
+   Console.WriteLine($"Число {number1} максимальное"); 
 }
-if (number2 > number3 && number3 < number2 )
+else if (number2 > number3 && number3 < number2 )
 {
-   Console.WriteLine($"Число {number2} больше чем число {number1} и больше чем число {number3}"); 
+   Console.WriteLine($"Число {number2} максимальное"); 
 }
-if (number3 > number2 && number1 < number3)
+else if (number3 > number2 && number1 < number3)
 {
-   Console.WriteLine($"Число {number3} больше чем число {number2} и больше чем число {number1}");
+   Console.WriteLine($"Число {number3} максимальное");
 }
+else Console.WriteLine("Введены некоректные числа");
