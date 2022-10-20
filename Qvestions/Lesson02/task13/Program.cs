70a254325cb861e670cd4464677e64a81e2c2f37
Vadim <mihailovvadick1996@yandex.ru>
2022-10-20 06:26:05 +0300

    change task13

--- Qvestions/Lesson02/task13/Program.cs
+++ Qvestions/Lesson02/task13/Program.cs
@@ -10,22 +10,37 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num > 99 && num < 1000)
 {
-int LastDigit = num % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
-Console.WriteLine($"Третьей  цифрой числа {num} является {LastDigit}");
+int Digit = num % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
+Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
 }
 else if (num >1000 && num <10000)
 {
-    int LastDigit = num / 10 % 10 ;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {LastDigit}");
+    int Digit = num / 10 % 10 ;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
 }
 else if (num >10000 && num <100000)
 {
-    int LastDigit = num / 10 / 10 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {LastDigit}");
+    int Digit = num / 10 / 10 % 10;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
 }
 else if (num >100000 && num <1000000)
 {
-    int LastDigit = num / 10 / 10 / 10 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {LastDigit}");
+    int Digit = num / 10 / 10 / 10 % 10;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+}
+else if (num >1000000 && num <10000000)
+{
+    int Digit = num / 10 / 10 / 10 / 10 % 10;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+}
+else if (num >10000000 && num <100000000)
+{
+    int Digit = num / 10 / 10 / 10 / 10 / 10 % 10;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+}
+else if (num >100000000 && num <1000000000)
+{
+    int Digit = num / 10 / 10 / 10 / 10 / 10 / 10 % 10;
+    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
 }
 else Console.WriteLine("Третьей цифры нет");
