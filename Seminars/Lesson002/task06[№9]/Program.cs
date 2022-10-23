6077725e2a62d706440ab3f3912584b46b19ba25
Vadim <mihailovvadick1996@yandex.ru>
2022-10-23 12:04:41 +0300

    change task06[№9]

--- Seminars/Lesson002/task06[№9]/Program.cs
+++ Seminars/Lesson002/task06[№9]/Program.cs
@@ -7,15 +7,30 @@ Console.WriteLine($"Сгенерировано число {num}");
 
 int FirstDigit = num / 10;
 int SecondDigit = num % 10;
-if(FirstDigit > SecondDigit)
-{
-    Console.WriteLine($"В числе {num} наибольшая цифра {FirstDigit}");
-}
-else if(FirstDigit < SecondDigit)
-{
-    Console.WriteLine($"В числе {num} наибольшая цифра {SecondDigit}");
-}
-else if(FirstDigit == SecondDigit) 
-{
-Console.WriteLine($"В числе {num} цифры равны ");
-}
+
+// if(FirstDigit > SecondDigit)
+// {
+//     Console.WriteLine($"В числе {num} наибольшая цифра {FirstDigit}");
+// }
+// else if(FirstDigit < SecondDigit)
+// {
+//     Console.WriteLine($"В числе {num} наибольшая цифра {SecondDigit}");
+// }
+// else if(FirstDigit == SecondDigit) 
+// {
+// Console.WriteLine($"В числе {num} цифры равны ");
+// }
+
+//          УСЛОВНЫЙ ОПЕРАТОР
+// int result = default;
+// if(FirstDigit > SecondDigit) result = FirstDigit;
+// else result = SecondDigit;
+// Console.WriteLine($"Наибольшая цифра {result}");
+
+//          ТЕРНАРНЫЙ ОПЕРАТОР 1
+// int maxDigit = FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+// Console.WriteLine($"В числе {num} наибольшая цифра {maxDigit}");
+
+//          ТЕРНАРНЫЙ ОПЕРАТОР 2
+Console.Write($"Наибольшая цифра {num} равна ");
+Console.WriteLine(FirstDigit > SecondDigit ? FirstDigit : SecondDigit);
