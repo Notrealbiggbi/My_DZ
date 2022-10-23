6a8e7351e71b48305d75f93f6d75a5998450305b
Vadim <mihailovvadick1996@yandex.ru>
2022-10-23 14:19:06 +0300

    change  task13

--- Qvestions/Lesson02/task13/Program.cs
+++ Qvestions/Lesson02/task13/Program.cs
@@ -7,40 +7,56 @@
 
 Console.WriteLine("Введите  число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+//              НЕОПТИМАЛЬНОЕ РЕШЕНИЕ
+// if (num > 99 && num < 1000)
+// {
+// int Digit = num % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
+// Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >1000 && num <10000)
+// {
+//     int Digit = num / 10 % 10 ;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >10000 && num <100000)
+// {
+//     int Digit = num / 100 % 10;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >100000 && num <1000000)
+// {
+//     int Digit = num / 1000 % 10;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >1000000 && num <10000000)
+// {
+//     int Digit = num / 10000 % 10;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >10000000 && num <100000000)
+// {
+//     int Digit = num / 100000 % 10;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else if (num >100000000 && num <1000000000)
+// {
+//     int Digit = num / 1000000 % 10;
+//     Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+// }
+// else Console.WriteLine("Третьей цифры нет");
 
-if (num > 99 && num < 1000)
+
+//              ОПТИМАЛЬНОЕ РЕШЕНИЕ
+int SecondDigit(int num )
 {
-int Digit = num % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
-Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
+    if (num > 99 && num < 1000) return num % 10;
+    if (num > 1000 && num < 10000) return num / 10 % 10 ;
+    if (num > 10000 && num < 100000) return num / 100 % 10;
+    if (num > 100000 && num < 1000000) return num / 1000 % 10;
+    return 0;
 }
-else if (num >1000 && num <10000)
-{
-    int Digit = num / 10 % 10 ;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else if (num >10000 && num <100000)
-{
-    int Digit = num / 100 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else if (num >100000 && num <1000000)
-{
-    int Digit = num / 1000 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else if (num >1000000 && num <10000000)
-{
-    int Digit = num / 10000 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else if (num >10000000 && num <100000000)
-{
-    int Digit = num / 100000 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else if (num >100000000 && num <1000000000)
-{
-    int Digit = num / 1000000 % 10;
-    Console.WriteLine($"Третьей  цифрой числа {num} является {Digit}");
-}
-else Console.WriteLine("Третьей цифры нет");
+int result = SecondDigit(num);
+string result1 = num > 99 && num < 1000000 
+                    ? $"Третьей  цифрой числа {num} является {result}" 
+                    : "Третьей цифры нет";
+Console.WriteLine(result1);
--- Seminars/Lesson002/task06[№9]/Program.cs
+++ Seminars/Lesson002/task06[№9]/Program.cs
@@ -5,8 +5,24 @@
 int num = new Random().Next(10,100);//10,11,12,13...99
 Console.WriteLine($"Сгенерировано число {num}");
 
-int FirstDigit = num / 10;
-int SecondDigit = num % 10;
+//      ИЗ МЕТОДА МОЖНО ВЫВОДИТЬ ГДЕ УГОДНО 
+// int result1 = MaxDigit(45);
+// Console.WriteLine($"Наибольшая цифра {45}  равна {result1}");
+// int result2 = MaxDigit(76);
+// Console.WriteLine($"Наибольшая цифра {76}  равна {result2}");
+// int result3 = MaxDigit(81);
+// Console.WriteLine($"Наибольшая цифра {81}  равна {result3}");
+// int result4 = MaxDigit(42);
+// Console.WriteLine($"Наибольшая цифра {42}  равна {result4}");
+// int result5 = MaxDigit(27);
+// Console.WriteLine($"Наибольшая цифра {27}  равна {result5}");
+// int result6 = MaxDigit(62);
+// Console.WriteLine($"Наибольшая цифра {62}  равна {result6}");
+// int result7 = MaxDigit(85);
+// Console.WriteLine($"Наибольшая цифра {85}  равна {result7}");
+
+// int FirstDigit = num / 10;
+// int SecondDigit = num % 10;
 
 // if(FirstDigit > SecondDigit)
 // {
@@ -32,5 +48,16 @@ int SecondDigit = num % 10;
 // Console.WriteLine($"В числе {num} наибольшая цифра {maxDigit}");
 
 //          ТЕРНАРНЫЙ ОПЕРАТОР 2
-Console.Write($"Наибольшая цифра {num} равна ");
-Console.WriteLine(FirstDigit > SecondDigit ? FirstDigit : SecondDigit);
+// Console.Write($"Наибольшая цифра {num} равна ");
+// Console.WriteLine(FirstDigit > SecondDigit ? FirstDigit : SecondDigit);
+
+//      МЕТОД
+int MaxDigit (int num)
+{
+    int FirstDigit = num / 10;
+    int SecondDigit = num % 10;
+    return FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+}
+
+int result = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра {num}  равна {result}");
