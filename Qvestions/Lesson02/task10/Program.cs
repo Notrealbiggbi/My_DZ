86ae464e659b5d8dbb26d27fab85a106b0e6cee8
Vadim <mihailovvadick1996@yandex.ru>
2022-10-23 14:27:19 +0300

    change task10

--- Qvestions/Lesson02/task10/Program.cs
+++ Qvestions/Lesson02/task10/Program.cs
@@ -6,11 +6,26 @@
 // 782 -> 8
 // 918 -> 1
 Console.WriteLine("Введите трёхзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int Num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 99 && num < 1000)
+
+//    МОЁ  НЕОПТИМАЛЬНОЕ РЕШЕНИЕ
+// if (num > 99 && num < 1000)
+// {
+// int SecondDigit = num / 10 % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
+// Console.WriteLine($"Второй цифрой числа {num} является {Digit}");
+// }
+// else Console.WriteLine("Введено некоректное число");
+
+//    МОЁ ОПТИМАЛЬНОЕ РЕШЕНИЕ
+
+int SecondDigit (int Num)
 {
-int Digit = num / 10 % 10; //   456 % 10 = 6     456 / 10 = 45 /10 = 4 / 10 = 0
-Console.WriteLine($"Второй цифрой числа {num} является {Digit}");
+   if (Num > 99 && Num < 1000) return Num / 10 % 10;
+   return 0; 
 }
-else Console.WriteLine("Введено некоректное число");
+int Result = SecondDigit (Num);
+string Final = Num > 99 && Num < 1000
+                    ? $"Второй  цифрой числа {Num} является {Result}" 
+                    : "Третьей цифры нет";
+Console.WriteLine(Final);
--- Qvestions/Lesson02/task13/Program.cs
+++ Qvestions/Lesson02/task13/Program.cs
@@ -6,7 +6,7 @@
 
 
 Console.WriteLine("Введите  число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int Num = Convert.ToInt32(Console.ReadLine());
 //              НЕОПТИМАЛЬНОЕ РЕШЕНИЕ
 // if (num > 99 && num < 1000)
 // {
@@ -47,16 +47,16 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 
 //              ОПТИМАЛЬНОЕ РЕШЕНИЕ
-int SecondDigit(int num )
+int SecondDigit(int Num )
 {
-    if (num > 99 && num < 1000) return num % 10;
-    if (num > 1000 && num < 10000) return num / 10 % 10 ;
-    if (num > 10000 && num < 100000) return num / 100 % 10;
-    if (num > 100000 && num < 1000000) return num / 1000 % 10;
+    if (Num > 99 && Num < 1000) return Num % 10;
+    if (Num > 1000 && Num < 10000) return Num / 10 % 10 ;
+    if (Num > 10000 && Num < 100000) return Num / 100 % 10;
+    if (Num > 100000 && Num < 1000000) return Num / 1000 % 10;
     return 0;
 }
-int result = SecondDigit(num);
-string result1 = num > 99 && num < 1000000 
-                    ? $"Третьей  цифрой числа {num} является {result}" 
+int Result = SecondDigit(Num);
+string Final = Num > 99 && Num < 1000000 
+                    ? $"Третьей  цифрой числа {Num} является {Result}" 
                     : "Третьей цифры нет";
-Console.WriteLine(result1);
+Console.WriteLine(Final);
