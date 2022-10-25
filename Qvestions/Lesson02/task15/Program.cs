7ea70d6e8cc3ca0c592ccbf198ce855467b15a4b
Vadim <mihailovvadick1996@yandex.ru>
2022-10-25 16:41:10 +0300

    change task15

--- Qvestions/Lesson02/task15/Program.cs
+++ Qvestions/Lesson02/task15/Program.cs
@@ -9,32 +9,18 @@
 
 
 Console.Write("Введите номер дня недели: ");
-int Num = Convert.ToInt32 (Console.ReadLine());
+int num = Convert.ToInt32 (Console.ReadLine());
 
-//     МОЁ НЕОПТИМАЛЬНОЕ РЕШЕНИЕ
-// if (num < 6)
-// {
-//     Console.WriteLine("Нет, сегодня будний день");
-// }
-// else if (num > 5 )
-// {
-//     Console.WriteLine("Да, сегодня выхоной");
-// }
-// else if(num > 7)
-// {
-//      Console.WriteLine("Такого дня недели нет");
-// }
-
-//    МОЁ ОПТИМАЛЬНОЕ РЕШЕНИЕ
-
-int DaysOfTheWeek (int Number)
+   
+if (num < 6)
 {
-   if (Number < 6) return 1;
-   if (Number > 5 ) return 2;
-   return 0;
+    Console.WriteLine("Нет, сегодня будний день");
 }
-int Result = DaysOfTheWeek(Num);
-string Final = Num > 0 && Num < 6 
-                    ? $"Нет, сегодня будний день" 
-                    : "Да, сегодня выхоной";
-Console.WriteLine(Final);
+else if (num > 5 )
+{
+    Console.WriteLine("Да, сегодня выхоной");
+}
+else if(num > 7)
+{
+     Console.WriteLine("Такого дня недели нет");
+}
