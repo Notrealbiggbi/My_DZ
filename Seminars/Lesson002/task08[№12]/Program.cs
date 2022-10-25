f9779b68101bd5a5f2d2c7c346ae2f8d7747012e
Vadim <mihailovvadick1996@yandex.ru>
2022-10-25 14:53:05 +0300

    create tasks 09[16],10[14]

---
+++ Seminars/Lesson002/task08[№12]/Program.cs
@@ -0,0 +1,28 @@
+﻿// Условие: Написать программу, которая 
+// 1. на входе принимает два числа и 
+// 2. выводить является ли второе число кратным первому.
+// 3. Если число 1 не кратно числу 2, то выводит остаток от деления
+
+
+Console.WriteLine("Введите первое число ");
+int FirstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int SecondDigit = Convert.ToInt32(Console.ReadLine());
+
+
+
+// if (FirstDigit % SecondDigit == 0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else Console.WriteLine($"Не кратно остаток {FirstDigit % SecondDigit}");
+
+bool Krat (int num1,int num2, int digit, int result)
+{
+    result =digit % num1;
+    result =digit % num1;
+    if (result  == 0) return true;
+    return false;
+}
+int Obt = Krat (FirstDigit,SecondDigit);
+string End = Krat ?"Кратно":"Не кратно остаток {FirstDigit % SecondDigit}";
---
+++ Seminars/Lesson002/task09[№16]/Program.cs
@@ -0,0 +1,21 @@
+﻿// Условие: Написаать программу, которая 
+// 1. на вход принимает два числа и 
+// 2. проверяет, является ли одно число квадратом другого.
+
+
+Console.WriteLine("Введите первое число ");
+int FirstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int SecondDigit = Convert.ToInt32(Console.ReadLine());
+
+
+
+if ( FirstDigit % SecondDigit == 0)
+{
+    Console.WriteLine("Да");
+}
+else if ( SecondDigit % FirstDigit == 0)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
