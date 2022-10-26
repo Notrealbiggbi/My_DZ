517bc16fcefde9570d4d547b0441853f5eecd5d7
Vadim <mihailovvadick1996@yandex.ru>
2022-10-26 10:26:45 +0300

    change str 11-14

--- Seminars/Lesson003/task08[№19]/Program.cs
+++ Seminars/Lesson003/task08[№19]/Program.cs
@@ -8,10 +8,10 @@ int quarter = Convert.ToInt32(Console.ReadLine());
 
 string Quarter(int quarterXY)
 {
-    if (quarterXY==1) return "x > 0 && y > 0";
-    if (quarterXY==2) return "x < 0 && y > 0";
-    if (quarterXY==3) return "x < 0 && y < 0";
-    if (quarterXY==4) return "x > 0 && y < 0";
+    if (quarterXY==1) return "x > 0 и y > 0";
+    if (quarterXY==2) return "x < 0 и y > 0";
+    if (quarterXY==3) return "x < 0 и y < 0";
+    if (quarterXY==4) return "x > 0 и y < 0";
     return "данные введены некорректно";
 }
 
