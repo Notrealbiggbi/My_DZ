6e83c423547499f298f7ffb4b51c4bda1a7f4fd4
Vadim <mihailovvadick1996@yandex.ru>
2022-10-26 14:54:49 +0300

    create task11[22]

---
+++ Seminars/Lesson003/task11[№22]/Program.cs
@@ -0,0 +1,21 @@
+﻿// Условие: Написать программу, которая 
+// 1. принимает на вход (N) и 
+// 2. выдаёт таблицу увадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 15, 25
+//2 -> 1, 4
+
+Console.WriteLine("Введите число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+void SquareT(int num)
+{
+    if (num>0)
+    {
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i}  {i*i}");
+    }
+    }
+}
+SquareT(Number);
