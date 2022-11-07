3f7e07eb175ac1511c780c4098d52639f96a37b4
Vadim <mihailovvadick1996@yandex.ru>
2022-11-07 11:59:11 +0300

    create task26 and task 28

---
+++ Seminars/Lesson004/task26/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// 1. на вход число и 
+// 2. выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int ColDig(int num)
+{
+    num = Math.Abs(num);
+    // Цикл WHILE
+    int count = 0;
+    while (num > 0)
+    {
+        // num = num / 10;
+        count++;
+        num /= 10;
+
+    }
+    return count > 0 ? count : 1;
+
+    // Цикл FOR
+    // int count = 0;
+
+    // for (int i = num; i > 0; i /= 10)
+    // {
+    //     count++;
+    // }
+    // return count > 0 ? count : 1;
+
+
+}
+
+
+int result = ColDig(number);
+Console.WriteLine($"колличество цифр в {number} -> {result}");
+
