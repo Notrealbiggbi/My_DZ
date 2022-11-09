ed7e4e09371fc63ceae89f20c864ff3524cb1e86
Vadim <mihailovvadick1996@yandex.ru>
2022-11-09 10:16:11 +0300

    change task29

--- Qvestions/Lesson04/task29/Program.cs
+++ Qvestions/Lesson04/task29/Program.cs
@@ -12,7 +12,7 @@ int[] RandommArray(int size)
     Random rnd = new Random();
     for (int num1 = 0; num1 < array.Length; num1++)
     {
-        array[num1]  = rnd.Next(0, 100);
+        array[num1]  = rnd.Next(-100, 100);
     }
     return array;
 }
