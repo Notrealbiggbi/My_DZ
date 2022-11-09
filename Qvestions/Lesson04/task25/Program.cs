9034ea5e68e47d0f32bb2a3623256a7835470598
Vadim <mihailovvadick1996@yandex.ru>
2022-11-09 10:14:21 +0300

    change task25

--- Qvestions/Lesson04/task25/Program.cs
+++ Qvestions/Lesson04/task25/Program.cs
@@ -4,32 +4,37 @@
 
 // 2, 4 -> 16
 
-//ВАРИАНТ ЧЕРЕЗ Math.
-Console.WriteLine("Введите число А: ");
+
+Console.WriteLine("Введите положительное число число А: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число В: ");
+Console.WriteLine("Введите положительное число В: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-double Expon(int number1, int number2)
-{
-    int a = number1;
-    int b = number2;
-    double exponentiation = Math.Pow(a, b); 
-    return exponentiation;
-}
-double Res = Expon(num1,num2);
-Console.Write($"{num1},{num2} -> {Res}");
-
-// ВАРИАНТ ЧЕРЕЗ FOR
-// int Expon(int number1, int number2)
+//ВАРИАНТ ЧЕРЕЗ Math.
+// double Expon(int number1, int number2)
 // {
-//     int exponetinetion = 1;
-//     for (int i = 0; i < number2; i++)
-//     {
-//        exponetinetion *= number1;
-//     }
-//     return exponetinetion;
+//     int a = number1;
+//     int b = number2;
+//     double exponentiation = Math.Pow(a, b); 
+//     return exponentiation;
 // }
-// double Res = Expon(num1, num2);
+// double Res = Expon(num1,num2);
 // Console.Write($"{num1},{num2} -> {Res}");
 
+// ВАРИАНТ ЧЕРЕЗ FOR
+int Expon(int number1, int number2)
+{
+    int exponetinetion = 1;
+    for (int i = 0; i < number2; i++)
+    {
+       exponetinetion *= number1;
+    }
+    return exponetinetion;
+}
+
+if (num1 > 0 && num2 > 0)
+{
+double Res = Expon(num1, num2);
+Console.Write($"{num1},{num2} -> {Res}");
+}
+else Console.WriteLine("Введено некоректное число ");
