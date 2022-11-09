71b8afef4332459c02f48384fa281bea1c5e0537
Vadim <mihailovvadick1996@yandex.ru>
2022-11-09 10:09:58 +0300

    change task29

--- Qvestions/Lesson04/task29/Program.cs
+++ Qvestions/Lesson04/task29/Program.cs
@@ -9,9 +9,10 @@ int num = 8;
 int[] RandommArray(int size)
 {
     int[] array = new int[size]; 
+    Random rnd = new Random();
     for (int num1 = 0; num1 < array.Length; num1++)
     {
-        array[num1]  = new Random().Next(0, 100);
+        array[num1]  = rnd.Next(0, 100);
     }
     return array;
 }
@@ -21,7 +22,8 @@ void PrintArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($",{array[i]}");
+        Console.Write(array[i]);
+        if (i<array.Length-1) Console.Write(",");
     }
     Console.Write("]");
     
