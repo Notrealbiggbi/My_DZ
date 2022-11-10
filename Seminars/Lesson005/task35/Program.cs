73af4d3e5b974b61ff941577c87740e9dcb4561d
Vadim <mihailovvadick1996@yandex.ru>
2022-11-10 12:55:55 +0300

    create task37

---
+++ Seminars/Lesson005/task35/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+
+int CountElements (int[]array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99) count++;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(123, 0, 200);
+PrintArray(arr);
+int countRes = CountElements(arr);
+Console.WriteLine($"Колличество эллементов массива, из отрезка [10,99] = {countRes}");
---
+++ Seminars/Lesson005/task37/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int[] MultiplayNumbers(int[] array)
+{
+    int size = array.Length /2;
+     if (array.Length % 2 == 1) size = size + 1;
+    int[] newArray = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        newArray[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 == 1) newArray[newArray.Length - 1] = array[array.Length / 2];
+        return newArray;
+}
+
+
+
+
+int[] arr = CreateArrayRndInt(5, 0, 10);
+PrintArray(arr);
+int[] res = MultiplayNumbers(arr);
+PrintArray(res);
+
+
+
+
+// double[] CreateArrayRndDouble(int size, int min, int max)
+// {
+//     double[] array = new double[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         double num = rnd.NextDouble() * (max - min) + min;
+//         array[i] = Math.Round(num, 1);
+//     }
+//     return array;
+// }
