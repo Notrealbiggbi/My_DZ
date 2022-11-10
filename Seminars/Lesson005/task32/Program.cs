4ca04ef16e9e0c9af9b8ba06a653afd49cd56dbb
Vadim <mihailovvadick1996@yandex.ru>
2022-11-10 11:55:38 +0300

    create task32

---
+++ Seminars/Lesson005/task32/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 32: 
+// 1. Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+
+int[] InversionArray(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10); // создаём переменную для запроса метода
+PrintArray(arr); // печатаем метод
+int[] reasultInversionArray = InversionArray(arr);
+PrintArray(reasultInversionArray);
---
+++ Seminars/Lesson005/task33/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNUmArray(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num) return true;
+    }
+
+    return false;
+}
+
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+Console.WriteLine("Введите искомое число");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (FindNUmArray(arr, number)) 
+// {
+//     Console.WriteLine("Да");
+// }
+// else 
+// {
+//     Console.WriteLine("Нет");
+// }
+
+Console.WriteLine(FindNUmArray(arr, number)?"Да": "Нет");
