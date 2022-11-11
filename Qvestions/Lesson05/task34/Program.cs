fd9f171a630a57b5e0796844d06f1eb1d88d8149
Vadim <mihailovvadick1996@yandex.ru>
2022-11-11 14:03:07 +0300

    create task34

---
+++ Qvestions/Lesson05/task34/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(8, 99, 1000);
+PrintArray(arr);
+int countRes = EvenNumbers(arr);
+Console.WriteLine($"Колличество чётных эллементов массива c положительным значением = {countRes}");
---
+++ Qvestions/Lesson05/task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: 
+// 1. Задайте одномерный массив, заполненный случайными числами. 
+// 2. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int[] GetSumNegativeElem(int[] array)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1) sumNegative += array[i];
+    }
+    return new int[] { sumNegative };
+}
+
+int[] arr = CreateArrayRndInt(13, 0, 10);
+PrintArray(arr);
+int[] countRes = GetSumNegativeElem(arr);
+Console.WriteLine($"Сумма нечётных  эллементов массива  = {countRes[0]}");
