71f4bf4fe8424e78c8c4c0a8e1e676ef7b7d54b3
Vadim <mihailovvadick1996@yandex.ru>
2022-11-14 22:28:19 +0300

    change str 17-18 in task43

--- Qvestions/Lesson06/task43/Program.cs
+++ Qvestions/Lesson06/task43/Program.cs
@@ -14,11 +14,11 @@ int k2 = Convert.ToInt32(Console.ReadLine());
 
 void lineIntersection(int B1, int K1, int B2, int K2)
 {
-    double num1 = K1 - K2;
-    double num2 = -B1 + B2;
+    double num1 = K2 - K1;
+    double num2 = B1 - B2;
     double x = num2 / num1;
     double y = K2 * x + B2;
-    Console.WriteLine($"b1 = {B1}, k1 = {K1}, b2 = {B2}, k2 = {K2} ->({Math.Round(x ,2, MidpointRounding.ToZero)}), ({Math.Round(y ,2, MidpointRounding.ToZero)}) ");
+    Console.WriteLine($"b1 = {B1}, k1 = {K1}, b2 = {B2}, k2 = {K2} ->({Math.Round(x, 2, MidpointRounding.ToZero)}), ({Math.Round(y, 2, MidpointRounding.ToZero)}) ");
 }
 
 lineIntersection(b1, k1, b2, k2);
