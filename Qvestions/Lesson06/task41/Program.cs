31635aa5af33edc9fc5dd01707b0166387ae2a6e
Vadim <mihailovvadick1996@yandex.ru>
2022-11-14 12:53:51 +0300

    create task41

---
+++ Qvestions/Lesson06/task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size, int min, int max) 
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int[] res = CreateArrayRndInt(5,3,9);
+PrintArray(res);
--- Seminars/Lesson006/task45/Program.cs
+++ Seminars/Lesson006/task45/Program.cs
@@ -3,3 +3,40 @@
 // копирования.
 
 
+int[] CreateArrayRndInt(int size, int min, int max) 
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10); // создаём переменную для запроса метода
+PrintArray(arr); 
+int[] arrNew = CopyArray(arr);
+arrNew[2]= 100;
+PrintArray(arrNew);
