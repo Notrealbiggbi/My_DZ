134ac28fe9f309af3396a3c76a607e7ba385a4b4
Vadim <mihailovvadick1996@yandex.ru>
2022-11-14 11:21:25 +0300

    create task42

--- Seminars/Lesson006/task42/Program.cs
+++ Seminars/Lesson006/task42/Program.cs
@@ -3,3 +3,36 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ConvertToBin(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 2;
+        result *= 10;
+        num /= 2;
+    }
+    return result;
+}
+
+int ReverseNum(int num)
+{
+    int result = 0;
+    while (num > 9)
+    {
+        result += num % 10;
+        result *= 10;
+        num /= 10;
+    }
+    result += num % 10;
+    return result;
+}
+
+int res = ConvertToBin(number);
+int result = ReverseNum(res);
+Console.Write(result);
