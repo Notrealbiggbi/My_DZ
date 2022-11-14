57251fdb284be2f12fe135eaddd361ef7e431e90
Vadim <mihailovvadick1996@yandex.ru>
2022-11-14 10:23:28 +0300

    create task39

---
+++ Seminars/Lesson006/task39/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+int[] CreateArrayRndInt(int size, int min, int max) // создаём метод где используем расчёт массива с случайными числавми
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // создаём метод с выводом прошлого метода
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    // [1 2 3 4 5] -> [5 4 3 2 1] по этой логике мы получим разворот массива
+}
+
+
+
+
+int[] arr = CreateArrayRndInt(5, 1, 9); // создаём переменную для запроса метода
+PrintArray(arr); // печатаем метод
+ReverseArray(arr);
+PrintArray(arr);
+// Array.Reverse(arr);          Есть метод встроенный с помощью которого можно разворачивать массив
+// PrintArray(arr);
