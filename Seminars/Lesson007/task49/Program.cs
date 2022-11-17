35eef2f59e6626bcff8557d6b4d2f7fc3ad8fc71
Vadim <mihailovvadick1996@yandex.ru>
2022-11-17 11:21:14 +0300

    create task49

---
+++ Seminars/Lesson007/task49/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5}   |");
+            else Console.Write($"{matrix[i,j], 5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+int[,] HonestlementsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i%2 == 0 && j%2 ==0)
+            {
+            matrix[i,j] *=  matrix[i,j];
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] mat = CreateMatrixRndInt(3, 4, 0, 10); // создаём переменную для запроса метода
+PrintMatrix(mat); // печатаем метод
+
+HonestlementsMatrix(mat);
+PrintMatrix(mat);
---
+++ Seminars/Lesson007/task51/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5}   |");
+            else Console.Write($"{matrix[i,j], 5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+int[,] SumElementsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+        }
+    }
+}
+
+
+
+int[,] mat = CreateMatrixRndInt(3, 4, 0, 10); // создаём переменную для запроса метода
+PrintMatrix(mat); // печатаем метод
