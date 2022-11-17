6d886daf2f1d03d1e8c2493eb08aa9f7bc56c460
Vadim <mihailovvadick1996@yandex.ru>
2022-11-17 15:38:57 +0300

    create task50

---
+++ Qvestions/Lesson07/task47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+double[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    double[,] matrix = new double[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            double num = rnd.NextDouble() * (max - min) + min;
+            matrix[i,j] = Math.Round(num, 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+double[,] mat = CreateMatrixRndInt(3, 4, 0, 10); // создаём переменную для запроса метода
+PrintMatrix(mat); // печатаем метод
---
+++ Qvestions/Lesson07/task50/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+int FindMatrixElements(int num1, int num2, int[,] matrix)
+{
+    int dig = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (num1 == i && num2 == j) return dig = matrix[i,j];
+        }
+    }
+    return dig;
+}
+
+
+int[,] mat = CreateMatrixRndInt(3, 4, 0, 10); // создаём переменную для запроса метода
+PrintMatrix(mat); // печатаем метод
+
+Console.Write("Введите позиции элемента в двумерном массиве через запятую :  ");
+string[] tokens = Console.ReadLine().Split(',');
+int number1 = int.Parse(tokens[0]);
+int number2 = int.Parse(tokens[1]);
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+int res = FindMatrixElements(number1, number2, mat);
+if(res == 0)
+{
+   Console.WriteLine($"{number1},{number2} -> такого элемента в массиве нет"); 
+}
+else Console.WriteLine($"{number1},{number2} -> эллемент {res}"); 
+
+// Console.WriteLine($"{num} -> такого элемента в массиве нет");
+// Console.WriteLine($"{num} -> эллемент {}");
