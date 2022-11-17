f0fa4ac1f56dfe7098496a7a12d2fdd6334da299
Vadim <mihailovvadick1996@yandex.ru>
2022-11-17 18:07:21 +0300

    change task52

--- Qvestions/Lesson07/task52/Program.cs
+++ Qvestions/Lesson07/task52/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, colums];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+double AverageMatrixRndIntColums(int[,] matrix)
+{
+    double average = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int col =  0;
+            col += j;
+            int sum =  ;
+            average =sum / col ;
+
+
+        }
+    }
+
+    return average;
+}
+
+
+int[,] mat = CreateMatrixRndInt(2, 2, 0, 4);
+PrintMatrix(mat);
+double res = AverageMatrixRndIntColums(mat);
+Console.WriteLine($"Среднее арифметическое каждого столбца: {Math.Round(res, 2, MidpointRounding.ToZero)}");
