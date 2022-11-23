5bc5a67a76cadb5e62386242bf3fddecfb8075e3
Vadim <mihailovvadick1996@yandex.ru>
2022-11-23 19:51:18 +0300

    create task54

---
+++ Qvestions/lesson08/task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+void StreamlineElementsOfRowsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1)-1 ; j++)
+        {
+            if (matrix[i, j + 1] > matrix[i, j])
+            {
+                int min = matrix[i, j];
+                matrix[i, j] = matrix[i, j + 1];
+                matrix[i, j + 1] = min;
+
+            }
+            
+        }
+    }
+}
+
+
+Console.WriteLine("Введите размер строк массива: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер столбцов массива: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] mat = CreateMatrixRndInt(number1, number2, 1, 10);
+PrintMatrix(mat);
+Console.WriteLine();
+StreamlineElementsOfRowsMatrix(mat);
+PrintMatrix(mat);
