dbc6b0222772e97a12f5f0569d6e2f9239c21cc3
Vadim <mihailovvadick1996@yandex.ru>
2022-11-26 18:22:27 +0300

    create task62

---
+++ Qvestions/lesson08/task62/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // создаём метод где используем расчёт двумерного массива с случайными числами
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// создаём метод с выводом прошлого метода
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}   |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+
+Console.WriteLine("Введите размер строк матрицы: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер столбцов матрицы: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] mat = CreateMatrixRndInt(number1, number2, 0, 10);
+PrintMatrix(mat);
