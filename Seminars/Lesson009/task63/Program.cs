e2bd3e67ca8ed9591d263e7a2e2b4dc4e5c03519
Vadim <mihailovvadick1996@yandex.ru>
2022-11-28 03:13:04 +0300

    create task63

---
+++ Seminars/Lesson009/task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63: Задайте значение N. Задайте программу, которая выдаёт все натуральные числа от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5." 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers (int num)
+{
+    if(num==0) return;
+    NaturalNumbers(num -1);
+    Console.Write($"{num} ");
+}
+NaturalNumbers(number);
