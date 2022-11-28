e34420584c7e1ef264670d53ac4798eee97c95a7
Vadim <mihailovvadick1996@yandex.ru>
2022-11-28 19:47:31 +0300

    create task67

--- Qvestions/Lesson09/task66/Program.cs
+++ Qvestions/Lesson09/task66/Program.cs
@@ -2,3 +2,21 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumElements(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 0) return 0;
+ else return n + SumElements(n-1);
+}
+for (int i = numM; i <= numN; i++)
+{
+ Console.WriteLine($"{i}! = {SumElements(i)}");
+}
+int res = SumElements(numN);
---
+++ Seminars/Lesson009/task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SumElements(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 0) return n;
+    return n % 10 + SumElements(n / 10);
+
+}
+
+int res = SumElements(number);
+Console.WriteLine($"Суммой всех эллементов в числе {number} является {res}");
